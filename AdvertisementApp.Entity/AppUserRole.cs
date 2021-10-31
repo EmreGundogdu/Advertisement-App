e897a83b0b3801a3676f2aa4aa7b2dc305e5dd41
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-31 14:07:29 +0300

    Advertisement class added

--- AdvertisementApp.Entity/AppUserRole.cs
+++ AdvertisementApp.Entity/AppUserRole.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdvertisementApp.Entity
 {
-    public class AppUserRole:BaseEntity
+    public class AppUserRole : BaseEntity
     {
         public string Username { get; set; }
         public string Password { get; set; }
