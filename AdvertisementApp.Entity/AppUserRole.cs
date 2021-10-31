3ffed40ae360f10d1ea431deedc2700be8490867
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-31 13:49:29 +0300

    AppUserRole added

--- AdvertisementApp.Entity/AppUserRole.cs
+++ AdvertisementApp.Entity/AppUserRole.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 
 namespace AdvertisementApp.Entity
 {
-    public class AppRole :BaseEntity
+    public class AppUserRole:BaseEntity
     {
-        public string Definition { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
         public List<AppUserRole> UserRoles { get; set; }
     }
 }
