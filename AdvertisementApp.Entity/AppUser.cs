e7edc6cfb7ce9c489522e6c8e45be442dfe67be7
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-31 13:47:29 +0300

    AppUser, AppRole & BaseEntity added

---
+++ AdvertisementApp.Entity/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Entity
+{
+    public class AppUser : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
