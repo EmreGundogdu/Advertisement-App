335d6dbeb9203d9e4cc6bc1cba8c811002aab6b3
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-01 23:07:48 +0300

    AppUserConfiguration added

--- AdvertisementApp.Entity/AppUser.cs
+++ AdvertisementApp.Entity/AppUser.cs
@@ -8,6 +8,8 @@ namespace AdvertisementApp.Entity
 {
     public class AppUser : BaseEntity
     {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
