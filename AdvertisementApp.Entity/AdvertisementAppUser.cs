c624674a77cb72c163a76bb0f525344067315bdd
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-01 13:55:03 +0300

    advertisementAppUser added

---
+++ AdvertisementApp.Entity/AdvertisementAppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Entity
+{
+    public class AdvertisementAppUser : BaseEntity
+    {
+        public int AdvertisementId { get; set; }
+        public int AppUserId { get; set; }
+        public int AdvertisementAppUserStatusId { get; set; }
+        public int StatusId { get; set; }
+        public int WorkExperience { get; set; }
+        public DateTime? EndDate  { get; set; }
+        public string CvPath { get; set; }
+    }
+}
