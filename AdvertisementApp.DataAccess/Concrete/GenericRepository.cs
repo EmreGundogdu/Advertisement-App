8e104e9ac27aa9d9a3c5ffaa4086fb1531cbab23
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-14 23:40:40 +0300

    GetAllAsync methods added with overloads

--- AdvertisementApp.DataAccess/Concrete/GenericRepository.cs
+++ AdvertisementApp.DataAccess/Concrete/GenericRepository.cs
@@ -1,9 +1,11 @@
 ﻿using AdvertisementApp.DataAccess.Context;
 using AdvertisementApp.DataAccess.Interface;
 using AdvertisementApp.Entity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +18,18 @@ namespace AdvertisementApp.DataAccess.Concrete
         public GenericRepository(AdvertiesmentContext context)
         {
             _context = context;
+        }
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _context.Set<T>().Where(expression).ToListAsync();
+        }
+        public async Task<List<T>> GetAllAsync<TKey>(Expression<Func<T,TKey>> selector) 
+        {
+            return await _context.Set<T>().OrderBy(selector).ToListAsync();
         }
     }
 }
