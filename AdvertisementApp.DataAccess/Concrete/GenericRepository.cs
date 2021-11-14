6cf0207dea908d9fc087ce101c06bd3071c5420a
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-14 23:32:50 +0300

    Generic Interface & class added

---
+++ AdvertisementApp.DataAccess/Concrete/GenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using AdvertisementApp.DataAccess.Context;
+using AdvertisementApp.DataAccess.Interface;
+using AdvertisementApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.Concrete
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly AdvertiesmentContext _context;
+
+        public GenericRepository(AdvertiesmentContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ AdvertisementApp.DataAccess/Interface/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using AdvertisementApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.Interface
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+
+    }
+}
