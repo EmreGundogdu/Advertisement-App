5bbb3bed42a039054ef3e0b960a13fef5d82f6cd
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-29 20:37:55 +0300

    unitOfWork added

---
+++ AdvertisementApp.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using AdvertisementApp.DataAccess.Interface;
+using AdvertisementApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T> GetRepository<T>() where T : BaseEntity;
+        Task SaveChangesAsync();
+    }
+}
---
+++ AdvertisementApp.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using AdvertisementApp.DataAccess.Concrete;
+using AdvertisementApp.DataAccess.Context;
+using AdvertisementApp.DataAccess.Interface;
+using AdvertisementApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AdvertiesmentContext _context;
+
+        public UnitOfWork(AdvertiesmentContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> GetRepository<T>() where T : BaseEntity
+        {
+            return new GenericRepository<T>(_context);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
