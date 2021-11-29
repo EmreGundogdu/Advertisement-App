536fe2866143fe8983433e158ab66863a4d4b5ac
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-29 20:33:47 +0300

    update,createasync, remove, getquery, findasync, getbyfilterasync methods are added to genericRepository

--- AdvertisementApp.DataAccess/Interface/IGenericRepository.cs
+++ AdvertisementApp.DataAccess/Interface/IGenericRepository.cs
@@ -1,7 +1,9 @@
-﻿using AdvertisementApp.Entity;
+﻿using AdvertisementApp.Common.Enums;
+using AdvertisementApp.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,15 @@ namespace AdvertisementApp.DataAccess.Interface
 {
     public interface IGenericRepository<T> where T : BaseEntity
     {
-
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+        Task<List<T>> GetAllAsync<TKey>(Expression<Func<T, TKey>> selector, OrderByType orderByType = OrderByType.DESC);
+        Task<List<T>> GetAllAsync<TKey>(Expression<Func<T, bool>> filter, Expression<Func<T, TKey>> selector, OrderByType orderByType = OrderByType.DESC);
+        Task<T> FindAsync(object id);
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter, bool asNoTracking = false);
+        IQueryable<T> GetQuery();
+        void Remove(T entity);
+        Task CreateAsync(T entity);
+        void Update(T entity, T unchanged);
     }
 }
