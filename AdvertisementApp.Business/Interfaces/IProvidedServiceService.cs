0b95e0e78f083ce25055b8cc45789a27851b6c87
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-30 23:01:15 +0300

    IprovidedeserviceService & ProvidedserviceService added

---
+++ AdvertisementApp.Business/Interfaces/IProvidedServiceService.cs
@@ -0,0 +1,14 @@
+﻿using AdvertisementApp.Dtos.ProvidedServiceDtos;
+using AdvertisementApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.Interfaces
+{
+    interface IProvidedServiceService : IGenericService<ProvidedServiceCreateDto, ProvidedServiceUpdateDto, ProvidedServiceListDto, ProvidedServices>
+    {
+    }
+}
