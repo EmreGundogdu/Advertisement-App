1de1d3de28e3e259102d2b6e64b0801a985d9676
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-11-30 23:10:53 +0300

    controller updated

--- AdvertisementApp.UI/Controllers/HomeController.cs
+++ AdvertisementApp.UI/Controllers/HomeController.cs
@@ -9,9 +9,16 @@ namespace AdvertisementApp.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IProvidedServiceService _providedServiceService
-        public IActionResult Index()
+        private readonly IProvidedServiceService _providedServiceService;
+
+        public HomeController(IProvidedServiceService providedServiceService)
         {
+            _providedServiceService = providedServiceService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _providedServiceService.GetAllAsync();
             return View();
         }
     }
