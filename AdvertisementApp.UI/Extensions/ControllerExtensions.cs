27f3bb0324ed125f09394e1b5ebefad8b325d0ee
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-12-02 21:51:00 +0300

    ResponseRedirectoAction method added

--- AdvertisementApp.UI/Extensions/ControllerExtensions.cs
+++ AdvertisementApp.UI/Extensions/ControllerExtensions.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using AdvertisementApp.Common;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AdvertisementApp.UI.Extensions
 {
-    public class ControllerExtensions
+    public static class ControllerExtensions
     {
+        public static IActionResult ResponseRedirectoAction(this Controller controller, IResponse<T> response, string actionName)
+        {
+            if (response.ResponseType == ResponseType.NotFound)
+                return controller.NotFound();
+            if (response.ResponseType == ResponseType.ValidationError)
+            {
+                foreach (var item in response.ValidationErrors)
+                {
+                    controller.ModelState.AddModelError(item.PropertyName, item.ErrorMessage)
+                }
+                return controller.View(response.Data);
+            }
+            return controller.RedirectToAction(actionName);
+        }
+        public static IActionResult ResponseView<T>(this Controller controller,)
     }
 }
