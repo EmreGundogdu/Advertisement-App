8aa88e87b80bb86a2e5fc9619af0e15d40eff203
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-04-17 18:22:40 +0300

    added AdvertisementAppUserCreateDtoValidator & dto

---
+++ AdvertisementApp.Business/Services/AdvertisementAppUserService.cs
@@ -0,0 +1,23 @@
+﻿using AdvertisementApp.DataAccess.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.Services
+{
+    public class AdvertisementAppUserService
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        public AdvertisementAppUserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task CreateAsync()
+        {
+
+        }
+    }
+}
---
+++ AdvertisementApp.Business/ValidationRules/FluentValidation/AdvertisementAppUserCreateDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using AdvertisementApp.Common.Enums;
+using AdvertisementApp.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.ValidationRules.FluentValidation
+{
+    public class AdvertisementAppUserCreateDtoValidator : AbstractValidator<AdvertisementAppUserCreateDto>
+    {
+        public AdvertisementAppUserCreateDtoValidator()
+        {
+            RuleFor(x => x.AdvertisementAppUserStatusId).NotEmpty();
+            RuleFor(x => x.AdvertisementId).NotEmpty();
+            RuleFor(x => x.AppUserId).NotEmpty();
+            RuleFor(x => x.CvFile).NotEmpty();
+            RuleFor(x => x.EndDate).NotEmpty().When(x => x.MilitaryStatusId == (int)MilitaryStatusType.Tecilli);
+            RuleFor(x => x.WorkExperience).NotEmpty();
+        }
+    }
+}
